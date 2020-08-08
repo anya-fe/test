f73d9e0a94d53bab33dcc5b6e34f42da0074d337
anna <anyabt19@gmail.com>
2020-08-08 17:30:16 +0300

    finished till related products

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,20 +1,53 @@
 <?php
 
 function load_my_css(){
-    wp_enqueue_style('./style.css');
+    wp_register_style('style', get_stylesheet_directory_uri() . './style.css',time() );
+    wp_enqueue_style('style');
  }
+
+function load_my_js(){
+    wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+    
+    wp_register_script('index', get_stylesheet_directory_uri() . '/index.js',array('jquery'), time(), true);
+    wp_enqueue_script('index');
+ }
+ add_action( 'wp_enqueue_scripts', 'load_my_js' );
+
+ function move_variation_price() {
+
+    remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation', 10 );
+    add_action( 'woocommerce_after_single_variation', 'woocommerce_single_variation', 10 );
+
+}
+add_action( 'woocommerce_before_add_to_cart_form', 'move_variation_price' );
+
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+add_action( 'woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 30 );
+
+add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs', 98 );
+
+function woo_remove_product_tabs( $tabs ) {
+
+    unset( $tabs['reviews'] ); 			// Remove the reviews tab
+    unset( $tabs['additional_information'] );  	// Remove the additional information tab
+
+    return $tabs;
+
+}
 
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_meta',40);
 add_action('woocommerce_single_product_summary','woocommerce_template_single_meta',8);
+
+remove_action('woocommerce_single_variation','woocommerce_single_variation',10);
+add_action('woocommerce_single_variation','woocommerce_single_variation',30);
+add_action('woocommerce_single_variation','woocommerce_single_variation_add_to_cart_button',20);
 
 
 
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price',10);
 add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price',20);
 
-
-remove_action('woocommerce_single_product_summary','woocommerce_template_single_excerpt',20);
-add_action('woocommerce_single_product_summary','woocommerce_template_single_excerpt',40);
 
 add_filter( 'woocommerce_get_availability', 'wcs_custom_get_availability', 1, 2);
 function wcs_custom_get_availability( $availability, $_product ) {
